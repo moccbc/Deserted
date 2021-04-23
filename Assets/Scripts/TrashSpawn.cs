1a0826a5f2b0595f9b2d383883e1f374802f3f10
Dilan-Wijesinghe <dilan.wijesinghe0@gmail.com>
2021-04-23 16:01:54 -0700

    Initial Commit

---
+++ Assets/Scripts/TrashSpawn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashSpawn : MonoBehaviour
+{
+    // Trash Spawner for our Trash Objects
+    // public lets it show up in the Unity Inspector
+    public GameObject Trash_Block;
+    public float repeatTime = 5f;
+    public const float COLUMNS = 23f; // 5 - 25
+    public const float ROWS = 21.15f; // -23 - 23
+
+    private System.Random random = new System.Random();
+   
+    void Start() {
+        InvokeRepeating("Spawn", 2f, repeatTime);
+    }
+        
+    void Spawn() {
+        float y = 10f;
+        float RandomRow = (float)random.Next(-(int)ROWS,(int)ROWS); // Generate a Random X value
+        float RandomCol = (float)random.Next(5, (int)COLUMNS); // Generate a Random Z value
+        GameObject newTrash = GameObject.Instantiate(Trash_Block);
+        newTrash.transform.position = new Vector3(RandomRow, y, RandomCol);
+    }
+}
